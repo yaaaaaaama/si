b6debbddf98dbe9d7fa580e503e94b946bc45444
katsu <yamadakatsuhiko@katsuMac.local>
2026-01-04 12:18:44 +0900

    remove 0.1 and 0.2

--- 0.2/config.js
+++
@@ -1,7 +0,0 @@
-// Supabase設定
-// XとYを実際のSupabase認証情報に置き換えてください
-
-window.SUPABASE_CONFIG = {
-  url: 'https://nzmtrhjezjnamtcpmqol.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56bXRyaGplempuYW10Y3BtcW9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY4NzAwMTEsImV4cCI6MjA4MjQ0NjAxMX0.3y-9ESCt9VUe8wpqhwKL2fLJDG2iKsbGiYzGk-K446s'
-};
